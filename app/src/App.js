52ff213dd3cbd76f1991070a1b081c7f5642816d
xavier <xavierhoskins@outlook.com>
2020-04-27 14:25:48 -0500

    added a login

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowesrRouter as Router, Route, Link } from 'react-router-dom';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>Water My Plants</h1>
-      <Route exact path="/Login" component={Login} />
+      {/* <h1>Water My Plants</h1> */}
+      <Route exact path="/" component={Login} />
     </div>
   );
 }
